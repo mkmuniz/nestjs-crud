ceb29978b8506266e1e1b4b1a270f3f3e82160b4
worstp <mikaelmuniz2001@gmail.com>
2022-10-04 11:13:38 -0300

    updating

--- src/series/serie.controller.ts
+++ src/series/serie.controller.ts
@@ -1,5 +1,5 @@
 import { SeriesService } from './serie.provider';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put } from '@nestjs/common';
 import { Serie } from './series.model';
 
 @Controller('series')
@@ -15,6 +15,16 @@ export class SeriesController {
   @Get(':id')
   async findOne(@Param('id') id: String): Promise<Serie> {
     return this.seriesService.getById(id);
+  }
+
+  @Put(':id')
+  async patchOne(@Param('id') id: String, @Param('serie') serie: Serie): Promise<Serie> {
+    return this.seriesService.patch(id, serie);
+  }
+
+  @Delete(':id')
+  async deleteOne(@Param('id') id: String): Promise<void> {
+    return this.seriesService.delete(id);
   }
 
 }
