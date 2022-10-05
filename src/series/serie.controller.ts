7cb41c2974d340f2bc6dcfe59b9fbd47895cc85a
worstp <mikaelmuniz2001@gmail.com>
2022-10-05 17:24:17 -0300

    feat: adding a logger to 'SeriesController'

--- src/series/serie.controller.ts
+++ src/series/serie.controller.ts
@@ -1,24 +1,27 @@
 import { SeriesService } from './serie.provider';
-import { Controller, Delete, Get, Param, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Logger, Param, Put } from '@nestjs/common';
 import { Serie } from './series.model';
 
 @Controller('series')
 export class SeriesController {
-
+  private readonly logger = new Logger(SeriesController.name);
   constructor(private seriesService: SeriesService) 
   {}
   @Get()
   async findAll(): Promise<Serie[]> {
+    this.logger.log('Getting all series');
     return this.seriesService.getAll();
   }
 
   @Get(':id')
   async findOne(@Param('id') id: String): Promise<Serie> {
+    this.logger.log('Getting a specific serie');
     return this.seriesService.getById(id);
   }
 
   @Put(':id')
   async patchOne(@Param('id') id: String, @Param('serie') serie: Serie): Promise<Serie> {
+    this.logger.log('Updating a serie');
     return this.seriesService.patch(id, serie);
   }
 
